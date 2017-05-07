caf4b5182ab2befb4a333681bc8ea72a041d1b64
macanother <macanother@macanother-PC>
2017-05-07 22:44:29 +0200

    add package test ; add package Junit

---
+++ src/OnlyForTest/test.java
@@ -0,0 +1,28 @@
+package OnlyForTest;
+
+import java.util.Hashtable;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test");
+		
+		//test: hold all key and value
+		Hashtable <String , Integer> test = new Hashtable<String, Integer>();
+		
+		//put first value
+		test.put("1", 1);
+		
+		//repeat put value, from 0 to 100
+		for(int i=0 ; i<100 ; i++){
+			test.put(String.valueOf(i), i);
+		}
+		
+		
+		//output
+		System.out.println(test.toString());
+		System.out.println(test.size());
+	}
+
+}
