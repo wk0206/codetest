eae1188114a00e54ce9e1ff1200d119cc7551bb7
macanother <macanother@macanother-PC>
2017-05-11 00:22:54 +0200

    add test folder

---
+++ test/test/AllTests.java
@@ -0,0 +1,12 @@
+package test;
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ Q001Test.class, Q231Test.class })
+public class AllTests {
+
+}
