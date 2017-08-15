9931ecf689725ad6acc564150cb6d06613c5b3bc
wang <wk0206@gmail.com>
2017-08-15 19:45:10 +0200

    Q066 Solution2

--- src/easy/Q066.java
+++ src/easy/Q066.java
@@ -34,11 +34,30 @@ public class Q066 {
         return digits;
     }
     
+    
+    public int[] solution2(int[] digits){
+    	
+    	for(int i = digits.length-1; i>=0; i--){
+    		if(digits[i]!=9){
+    			digits[i]++;
+    			break;
+    		}
+    		else digits[i]=0;
+    	}
+    	
+    	if(digits[0]==0){
+    		int[] res = new int[digits.length+1];
+    		res[0]=1;
+    		return res;
+    	}
+    	return digits;
+    }
+    
     public static void main(String[] args){
     	Q066 instance = new Q066();
     	
     	int[] digits = new int[]{9,9,9,9};
-    	int[] res = instance.plusOne(digits);
+    	int[] res = instance.solution2(digits);
     	System.out.println(Arrays.toString(res));
     }
 }
