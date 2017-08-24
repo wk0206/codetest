182f849c4e689c569b2a4a016d2189e59acfb148
wang <wk0206@gmail.com>
2017-08-24 22:05:08 +0200

    Q009

--- src/easy/Q009.java
+++ src/easy/Q009.java
@@ -4,4 +4,30 @@ package easy;
 //Palindrome Number
 public class Q009 {
 
+	
+    public boolean isPalindrome(int x) {
+        if(x<0|| (x!=0 &&x%10==0)) return false;
+        int sum=0;
+        while(x>sum)
+        {
+        	System.out.println("sum "+sum);
+        	System.out.println("x   "+x);
+            sum = sum*10+x%10;
+            x = x/10;
+        }
+        
+        System.out.println("sum "+sum);
+    	System.out.println("x   "+x);
+        return (x==sum)||(x==sum/10);
+    }
+    
+    public static void main(String[] args){
+    	Q009 instance = new Q009();
+    	
+    	int s = 1234554321;
+    	boolean res = instance.isPalindrome(s);
+    	
+    	System.out.println(res);
+    	//System.out.println("a".indexOf(" "));
+    }
 }
