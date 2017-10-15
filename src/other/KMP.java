4303a02afb85a1bd5f071935c04d8b3b4ee6cdec
macanother <macanother@macanother-PC>
2017-10-15 16:42:07 +0200

    KMP

---
+++ src/other/KMP.java
@@ -0,0 +1,57 @@
+package other;
+
+public class KMP {
+	
+	public int[] FF(String W){
+		int i = 2;
+		int j = 0;
+		
+		int[] T = new int[W.length()];
+		T[0]=-1;
+		T[1]=0;
+		
+		while(i<W.length()){
+			//equal, continue
+			if(W.charAt(i-1)==W.charAt(j)){
+				T[i]=j+1;
+				i++;
+				j++;
+			}else if(j>0){
+				j=T[i-1];
+				
+			}else{
+				T[i]=0;
+				i++;
+			}
+		}
+		
+		return T;
+	}
+
+	public int KMP(String S, String W){
+		int[] T = FF(W);
+		int pS = 0;
+		int pW = 0;
+ 
+		while(pS+pW <= S.length()){
+			if(S.charAt(pS+pW)==W.charAt(pW)){
+				pW++;
+				if(pW==W.length()) return pS;
+			}else {
+				//not equal, where to start again
+				pS=pS+pW-T[pW];
+				if(pW>0){
+					pW=T[pW];
+				}
+			}
+		}
+		
+		return -1;
+	}
+	
+	
+	public void main(String[] args){
+		
+		
+	}
+}
