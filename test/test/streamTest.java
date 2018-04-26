130004b9f7e3ecad92f78eb85c1ae8fbcca8fbf0
zbook15G3 <zbook15G3@192.168.0.164>
2018-04-26 10:32:13 +0200

    stream

--- test/test/streamTest.java
+++ test/test/streamTest.java
@@ -3,6 +3,7 @@ package test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class streamTest {
@@ -12,6 +13,8 @@ public class streamTest {
 
 		List<String> a = new ArrayList<>();
 		Arrays.asList(a);
+		
+		int[] newArray = new int[] {1,2,3,4};
 		
 		int[] array = new int[4];
 		array[0] = 0;
@@ -27,10 +30,26 @@ public class streamTest {
 		intA.add(2);
 		intA.add(3);
 		
+		Arrays.asList(array);
+		
 		intA.stream().mapToDouble(item->item).forEach(System.out::println);
 		
 		
 		Stream.of(1,2,3,4,5).map(item->item*item).forEach(System.out::println);;
+		
+		
+		List<String> strList = new ArrayList<>();
+		strList.add("abc");
+		strList.add("def");
+		strList.add("");
+		strList.add("efg");
+		strList.add("hi");
+		
+		
+		// 创建一个字符串列表，每个字符串长度大于2
+		List<String> filtered = strList.stream().filter(x -> x.length()> 2).collect(Collectors.toList());
+		System.out.printf("Original List : %s, filtered list : %s %n", strList, filtered);
+		
 	}
 
 }
