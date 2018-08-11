409aac8820dcb82828d382e770ec25d8f50b5884
zbook15G3 <zbook15G3@192.168.0.164>
2018-08-11 16:07:31 +0200

    LEET CODE

---
+++ src/mid/Q081.java
@@ -0,0 +1,14 @@
+package mid;
+
+public class Q081 {
+
+	//use two pointer
+	//use binary search
+	//O logn
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
